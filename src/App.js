7dabd022420534d03f4782df4e9f4212231364bd
treecoww <tcao89@gmail.com>
2023-05-03 08:47:59 -0500

    import sample logos & assets

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navigation from "./Components/Navigation";
 // import Header from "./Components/Header";
 // import Footer from "./Components/Footer";
+import "./App.scss"
 
 
 // setting the header in footer in the components file for my dynamic editing
