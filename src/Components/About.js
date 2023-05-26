be33020f4aba04bcb7d6e0eafbf97d4f05d7e3a5
treecoww <tcao89@gmail.com>
2023-05-25 20:04:36 -0500

    add about section

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,1 +1,22 @@
 //add a photo & biography
+
+// Recent Photo or Avatar
+// Short Biography
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <p className="content is-medium">Customer Success Manager</p>
+      <hr />
+      <p className="content is-italic mt-4">
+        I'm Tree, a Customer Success Manager with developer skills.
+      </p>
+      <p className="content">
+        My core job is Chao Coordinator. My day-to-day varies. Most days are spent managing 3 teams & collaborating cross-company for various needs. I love that my job is flexible and allows for growth.
+      </p>
+    </div>
+  );
+}
+
+export default About;
