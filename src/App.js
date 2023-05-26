5b01ca10f90e7e21a7b5fc552c147669afaf3a18
treecoww <tcao89@gmail.com>
2023-05-26 18:29:29 -0500

    update navbar casing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import About from "./Components/About";
 import Contact from "./Components/Contact";
-import Navbar from "./Components/NavBar";
+import NavBar from "./Components/NavBar";
 import Projects from "./Components/Projects";
 import Skills from "./Components/Skills";
 
 export default function App() {
   return (
     <main>
-      <Navbar />
+      <NavBar />
       <About />
       <Projects />
       <Skills />
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Navbar() {
+export default function NavBar() {
   return (
     <header className="bg-gray-800 md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
