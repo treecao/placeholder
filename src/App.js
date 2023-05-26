9ab1977b04f2bc109e721eaa47ecde04922afc39
treecoww <tcao89@gmail.com>
2023-05-26 05:00:17 -0500

    add addtl packages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from "react";
-import Navigation from "./Components/Navigation";
+// import Navigation from "./Components/Navigation";
 // import Header from "./Components/Header";
 // import Footer from "./Components/Footer";
-import "./App.scss"
+// import "./App.scss"
 
 
 // setting the header in footer in the components file for my dynamic editing
-function App() {
-  return (
-    <div>
-      <Navigation />
-      {/* <Header /> */}
-      {/* <Footer /> */}
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div>
+//       <Navigation />
+//       {/* <Header /> */}
+//       {/* <Footer /> */}
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
+
+export default function App() {
+  return "App";
+}
