dd0564c20f2f72ab8e74fe17b70577a54a2346a6
treecoww <tcao89@gmail.com>
2023-05-26 05:01:31 -0500

    update react export

--- src/App.js
+++ src/App.js
@@ -19,5 +19,14 @@ import React from "react";
 // export default App;
 
 export default function App() {
-  return "App";
+  return (
+    <main>
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
+  );
 }
