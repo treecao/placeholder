ad3f71f9c3d3ec1839130ac42e7d8a18375bd644
treecoww <tcao89@gmail.com>
2023-05-25 23:16:54 -0500

    update projects & portfolio

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,1 +1,129 @@
-//display projects 
+import emoji from "react-easy-emoji";
+
+const greeting = {
+  username: "Tree",
+  title: "Hi, I'm Tree",
+  subTitle: emoji(
+    "I'm just learning to do the things"
+  ),
+  displayGreeting: true 
+};
+
+const socialMediaLinks = {
+  github: "https://github.com/treecao",
+  linkedin: "http://www.linkedin.com/in/treecoww",
+
+  display: true 
+};
+
+// Education Section
+
+const educationInfo = {
+  display: true, // Set false to hide this section, defaults to true
+  schools: [
+    {
+      schoolName: "The University of Texas at Austin",
+      logo: require("./assets/images/UTaustinlogo.png"),
+      subHeader: "Full Stack Software Engineering Bootcamp",
+      duration: "November 2022 - May 2023",
+      desc: "Earned a certificate in full stack web development",
+      descBullets: [
+        "",
+        "Acquire skills using NoSQL databases, convert traditional applications into progressive web applications, and learn React."
+      ]
+    },
+    {
+      schoolName: "The University of North Texas",
+      logo: require("asset logo"),
+      subHeader: "Interdisciplinary Studies",
+      desc: "Minors:",
+      descBullets: ["English"]
+    }
+  ]
+};
+
+const workExperiences = {
+  display: true, 
+  experience: [
+    {
+      role: "",
+      company: "",
+      companylogo: require(""),
+      date: "",
+      desc: "",
+      descBullets: [
+        "things"
+      ]
+    },
+    {
+      role: "",
+      company: "",
+      companylogo: require(""),
+      date: "",
+      desc: "",
+      descBullets: [
+        "things"
+      ]
+    }
+  ]
+};
+
+const treeProjects = {
+  title: "Projects",
+  subtitle:
+    "Recent Collaborations",
+  projects: [
+    {
+      image: require("./assets/images/momart.png"),
+      projectName: "MoMart",
+      projectDesc:
+        "An ecommerce website using React.",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "https://momart.herokuapp.com/"
+        },
+        {
+          name: "Visit Repository",
+          url: "https://github.com/romanjamess/MoMart"
+        }
+      ]
+    },
+    {
+      image: require("./assets/images/Thingamabobs.png"),
+      projectName: "Thingamabobs",
+      projectDesc:
+        "An e-commerce website that allows users to purchase and sell items of their choice.",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "https://treethingamabobs.herokuapp.com/"
+        },
+        {
+          name: "Visit Repository",
+          url: "https://github.com/treecao/thingamabobs/"
+        }
+      ]
+    },
+  ],
+  display: true 
+};
+
+const contactInfo = {
+  title: emoji("Contact Me ☎️"),
+  subtitle:
+    "text here",
+  number: "fakenumber",
+  email_address: "treecoww@ymail.com"
+};
+
+
+
+export {
+  greeting,
+  socialMediaLinks,
+  educationInfo,
+  workExperiences,
+  treeProjects,
+  contactInfo,
+};
