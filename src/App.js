7d0a5bb047dee9bba302c77099fcd64e213931e3
treecoww <tcao89@gmail.com>
2023-05-26 05:12:25 -0500

    import components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,26 @@
 import React from "react";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Navbar from "./Components/NavBar";
+import Projects from "./Components/Projects";
+import Skills from "./Components/Skills";
+import Testimonials from "./Components/Testimonials";
+
+export default function App() {
+  return (
+    <main>
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
+  );
+}
+
+
+
 // import Navigation from "./Components/Navigation";
 // import Header from "./Components/Header";
 // import Footer from "./Components/Footer";
@@ -17,16 +39,3 @@ import React from "react";
 // }
 
 // export default App;
-
-export default function App() {
-  return (
-    <main>
-      <Navbar />
-      <About />
-      <Projects />
-      <Skills />
-      <Testimonials />
-      <Contact />
-    </main>
-  );
-}
